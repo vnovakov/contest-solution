a27616bebd4973c095b7b11c7834ad78b39a1405
Vladimir Novakovski <vladimir@addepar.com>
2015-07-08 17:38:39 -0700

    Add another Polish problem

---
+++ poland/wag.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> a;
+	for (int i = 0; i < n; i++) {
+		int item;
+		cin >> item;
+		a.push_back(item);
+	}
+	
+	
+
+	return 0;
+}
